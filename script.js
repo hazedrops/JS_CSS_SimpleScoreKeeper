2e2cafcb2887d6752b84a29695b80a93203adca5
hazedrops <hazedrops@hotmail.com>
2022-08-12 15:17:45 -0700

    Added color change functionality and continuous play feature

--- script.js
+++ script.js
@@ -5,32 +5,96 @@ const playerOne = document.getElementById('playerOne')
 const playerTwo = document.getElementById('playerTwo')
 const reset = document.getElementById('reset')
 
-let goal = 1;
+let goal = 1
+let p1Score = 0
+let p2Score = 0
+let finished = false
 
 targetPtr.addEventListener('click', () => {
-  goal = targetPtr.value;
-  clearScore();
+  clearScore()
+
+  finished = false;
+
+  goal = parseInt(targetPtr.value)
 })
 
 playerOne.addEventListener('click', () => {
-  updateScore(pOneScore)
+  if (!finished) {
+    p1Score++
+    updateScore(pOneScore, p1Score)
+
+    if (p1Score === goal) {
+      changeScoreColor(pOneScore)
+      finished = true
+    }
+  }
 })
 
 playerTwo.addEventListener('click', () => {
-  updateScore(pTwoScore)
+  if (!finished) {
+    p2Score++
+    updateScore(pTwoScore, p2Score)
+
+    if (p2Score === goal) {
+      changeScoreColor(pTwoScore)
+      finished = true
+    }
+  }
 })
 
 reset.addEventListener('click', () => {
-  clearScore();
+  clearScore()
+
+  finished = false
 })
 
-function updateScore(pScore) {
-  if (parseInt(pScore.innerHTML) < goal) {
-    pScore.innerHTML = parseInt(pScore.innerText) + 1
+function updateScore(pScore, score) {
+  pScore.textContent = score
+
+  if (pScore.id === 'pOneScore') {
+    p1Score = score
+  } else {
+    p2Score = score
+  }
+
+  if (p1Score === goal) {
+    changeScoreColor(pScore)
+    finished = true;
+  }
+
+  if (p2Score === goal) {
+    changeScoreColor(pScore)
+    finished = true
   }
 }
 
-function clearScore() {
-  pOneScore.innerHTML = 0
-  pTwoScore.innerHTML = 0
+function changeScoreColor(pScore) {
+  let sibling
+
+  if (pScore.id === 'pOneScore') {
+    sibling = pScore.nextElementSibling
+    // console.log('nextElem is: ', sibling)
+  } else {
+    sibling = pScore.previousElementSibling
+    // console.log('prevElem is: ', sibling)
+  }
+
+  pScore.style.color = 'green'
+  sibling.style.color = 'red'
 }
+
+function clearScore() {
+  pOneScore.textContent = 0
+  pTwoScore.textContent = 0
+
+  p1Score = 0
+  p2Score = 0
+
+  setColor('black')
+}
+
+function setColor(color) {
+  pOneScore.style.color = color
+  pTwoScore.style.color = color
+}
+
